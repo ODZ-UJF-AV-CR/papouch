636c25d2e5a0ef32087685a0da4b5d0e2d27ff9a
user <Vaclav Stepan>
2019-06-13 15:56:40 +0200

    190604 version of the anemometer script

--- papouch.sh
+++ papouch.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/bash
 
-INTERVAL=5
+INTERVAL=60
 echo Recording from Papouch every $INTERVAL\ seconds.
 echo Divide speeds by 10 to get m/s.
 echo
 
-mkdir -p data
-mkdir -p data/xml
+DD=papouch
+XML=$DD/xml
+mkdir -p $DD
+mkdir -p $XML
 
 while ( true )
 do
 	DT=$( /usr/bin/date +%Y-%m-%d-%H-%M-%S )
 	DR=$( /usr/bin/date +"%Y-%m-%d %H:%M:%S" )
-	wget http://192.168.1.254/fresh.xml -O data/xml/${DT}.xml
+	wget http://192.168.1.254/fresh.xml -O $XML/${DT}.xml
 	( 
 	 echo -en $DR\\t 
-	 cat data/xml/${DT}.xml | sed 's/^M$//' | grep sns | sed -r 's/.*type="6".*val="([0-9]+)".*vals="([A-Z]+)".*type="7".*val="([0-9]+)".*/dir \1\t\2\tspd\t\3/' 
-	) | tee -a data/log.txt
+	 cat $XML/${DT}.xml | sed 's/^M$//' | grep sns | sed -r 's/.*type="6".*val="([0-9]+)".*vals="([A-Z]+)".*type="7".*val="([0-9]+)".*/dir \1\t\2\tspd\t\3/' 
+	) | tee -a $DD/log.txt
 	sleep $INTERVAL
 done
