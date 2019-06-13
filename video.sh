3a64cfaffc96f06d30bc0dea83483808a0d502e6
user <Vaclav Stepan>
2019-06-13 16:31:59 +0200

    Seems to be working, including streaming

--- video.sh
+++ video.sh
@@ -13,9 +13,10 @@ do
 	echo Do vlc http://localhost:8099 to watch stream. Restarts every $TIMEOUT seconds
 
 	( echo Will kill in $TIMEOUT seconds ; sleep $TIMEOUT ; killall vlc ) &
-	#( sleep 10 ; nice vlc http://localhost:8099 ) &
+	( sleep 5 ; nice vlc http://localhost:8099 ) &
 	# cvlc v4l2:///dev/video0:chroma=h264 :input-slave=alsa://hw:1,0 --sout '#transcode{acodec=mpga,ab=128,channels=2,samplerate=44100,threads=4,audio-sync=1}:standard{access=file,mux=mp4,dst="'$DT'.mp4"}'
-	cvlc v4l2:///dev/video0:chroma=h264 :input-slave=alsa://hw:1,0 --sout '#transcode{acodec=mpga,ab=128,channels=2,samplerate=44100,threads=4,audio-sync=1}:duplicate{dst=standard{access=file,mux=mp4,dst="'$DT'.mp4"},dst=standard{access=http,mux=ts,mime=video/ts,dst=:8099}}' 
+	cvlc --run-time=10 v4l2:///dev/video0:chroma=h264 :input-slave=alsa://hw:1,0 --sout '#transcode{acodec=mpga,ab=128,channels=2,samplerate=44100,threads=4,audio-sync=1}:duplicate{dst=standard{access=file,mux=mp4,dst="'$DT'.mp4"},dst=standard{access=http,mux=ts,mime=video/ts,dst=:8099}}' 
 	echo ----------------------------------------------------------
+	echo
+	wait
 done
-
